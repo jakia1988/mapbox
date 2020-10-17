9362298ab0f069daa052f5ab32554ac9d651d407
Shahid Khan <shahid.k@clik.ai>
2020-10-17 16:24:30 +0530

    Annotation added

--- Annotation.js
+++ Annotation.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import { Animated, View, Text, StyleSheet, Image } from 'react-native';
+import React, { useCallback } from 'react';
+import { Animated, View, Text, StyleSheet, Image, Dimensions } from 'react-native';
 import MapboxGL from '@react-native-mapbox-gl/maps';
+const singleImage = require('./place-marker.png');
+const multipleImage = require('./marker.png');
 
 
 const ANNOTATION_SIZE = 45;
@@ -28,56 +30,78 @@ const styles = StyleSheet.create({
     },
 });
 
+
+const layerStyles = {
+
+    singlePointImg: {
+        iconImage: singleImage,
+        textPitchAlignment: 'map',
+    },  
+
+    multipleImagePoint: {
+        iconImage: multipleImage,
+        textPitchAlignment: 'map',
+    },
+  
+    clusterCount: {
+      textField: '{point_count}',
+      textSize: 16,
+      iconImage: multipleImage,
+      textPitchAlignment: 'map',
+      textColor: 'black'
+    },
+  };
+  
+
 export default function Annotation() {
     const mapRef = React.useRef(null)
-    const pointerRef = React.useRef(null)
-
-    React.useEffect(() => {
-        console.log(mapRef.current)
-    }, [mapRef])
+    const [currentLocation, setCurrentLocation] = React.useState('')
+     
+    const onAnnotationPress = React.useCallback((args) => {
+       const {coordinates: {latitude, longitude}} = args;
+       setCurrentLocation(`${latitude}, ${longitude}`)
+    }, [])
 
     return (
         <>
             <MapboxGL.MapView
                 ref={(c) => (mapRef.current = c)}
-                onPress={(e) => console.log('On Press', e)}
-                onDidFinishLoadingMap={(e) => { console.log(e) }}
                 style={{ flex: 1, fillColor: '#f4efe8' }}
+                styleURL={MapboxGL.StyleURL.Light}
             >
                 <MapboxGL.Camera
-                    zoomLevel={16}
-                    centerCoordinate={[-73.99155, 40.73581]}
+                    zoomLevel={6}
+                    pitch={45}
+                    centerCoordinate={[-117.6988297,35.8571663,5.21]}
                 />
 
-                <MapboxGL.PointAnnotation
-                    id="annotation"
-                    coordinate={[-73.99155, 40.73581]}
-                    title="Annotation View"
-                    onDrag={(e) =>
-                        console.log('onDrag:', e.properties.id, e.geometry.coordinates)
-                    }
-                    onDragStart={(e) =>
-                        console.log('onDragStart:', e.properties.id, e.geometry.coordinates)
-                    }
-                    onDragEnd={(e) =>
-                        console.log('onDragEnd:', e.properties.id, e.geometry.coordinates)
-                    }
-                    ref={(c) => (pointerRef.current = c)}>
-                    <View style={styles.annotationContainer}>
-                        <Image
-                            source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}
-                            style={{ width: ANNOTATION_SIZE, height: ANNOTATION_SIZE }}
-                            onLoad={() => pointerRef.current.refresh()}
-                        />
-                    </View>
-                    <MapboxGL.Callout>
-                        <View style={{position: 'absolute', bottom: 0, backgroundColor: 'red'}}>
-                             <Text>Hi hidden msg</Text>
-                        </View>
-                    </MapboxGL.Callout>
-                </MapboxGL.PointAnnotation>
-            </MapboxGL.MapView>
+                <MapboxGL.ShapeSource
+                    id="earthquakes"
+                    cluster
+                    clusterRadius={50}
+                    clusterMaxZoom={14}
+                    onPress={(e) => onAnnotationPress(e)}
+                    url="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+                    >
 
+                    <MapboxGL.SymbolLayer
+                      id="pointCount"
+                      style={layerStyles.clusterCount}
+                    />
+
+                    <MapboxGL.SymbolLayer  
+                        id="singlePoint"
+                        filter={['!', ['has', 'point_count']]}
+                        style={layerStyles.singlePointImg}
+                    />
+
+                        
+                    </MapboxGL.ShapeSource>
+     
+            </MapboxGL.MapView>
+            <View style={{position:'absolute', flex: 1, justifyContent: 'center', alignItems:'center', width: Dimensions.get('window').width, height: 100, bottom: 0, backgroundColor: '#fff'}}>
+                <Text>{currentLocation || 'Please select any location'}</Text>
+            </View>          
 
         </>
     )
